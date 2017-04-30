fa40dc71216ed2deeb4bf1f26eea1ff2583e8aa1
David Tong <david.truong.tong@gmail.com>
2017-05-01 01:01:57 +0700

    - [UI]: Implement Enter phone number

---
+++ Muslims/Controller/EnterPhone/EnterPhoneViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterPhoneViewController.h
+//  Muslims
+//
+//  Created by David Tong on 5/1/17.
+//  Copyright © 2017 Muslims. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface EnterPhoneViewController : BaseViewController
+
+@end
---
+++ Muslims/Controller/EnterPhone/EnterPhoneViewController.m
@@ -0,0 +1,25 @@
+//
+//  EnterPhoneViewController.m
+//  Muslims
+//
+//  Created by David Tong on 5/1/17.
+//  Copyright © 2017 Muslims. All rights reserved.
+//
+
+#import "EnterPhoneViewController.h"
+
+@interface EnterPhoneViewController ()
+
+@end
+
+@implementation EnterPhoneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
