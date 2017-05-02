ae8838f9a39672dbf0094efffac09777bb522562
David Tong <david.truong.tong@gmail.com>
2017-05-03 00:12:53 +0700

    [UI]: My profile screen

---
+++ Muslims/Controller/Profile/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Muslims
+//
+//  Created by David Tong on 5/2/17.
+//  Copyright © 2017 Muslims. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ProfileViewController : BaseViewController
+
+@end
---
+++ Muslims/Controller/Profile/ProfileViewController.m
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.m
+//  Muslims
+//
+//  Created by David Tong on 5/2/17.
+//  Copyright © 2017 Muslims. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
