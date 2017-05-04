36d26bff339851249f93ac450463a5d0f85d996c
David Tong <david.truong.tong@gmail.com>
2017-05-05 00:12:13 +0700

    [UI]: Implement swipe card view

---
+++ Muslims/Controller/Dashboard/CardView.m
@@ -0,0 +1,26 @@
+//
+//  CardView.m
+//  ZLSwipeableViewDemo
+//
+//  Created by Zhixuan Lai on 11/1/14.
+//  Copyright (c) 2014 Zhixuan Lai. All rights reserved.
+//
+
+#import "CardView.h"
+
+@implementation CardView
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.layer.shadowOpacity = 0.33;
+    self.layer.shadowOffset = CGSizeMake(0, 1.5);
+    self.layer.shadowRadius = 4.0;
+    self.layer.shouldRasterize = YES;
+    self.layer.rasterizationScale = [UIScreen mainScreen].scale;
+    
+    // Corner Radius
+    self.layer.cornerRadius = 10.0;
+}
+
+@end
--- Muslims/Controller/Dashboard/DashboardViewController.m
+++ Muslims/Controller/Dashboard/DashboardViewController.m
@@ -9,21 +9,74 @@
 #import "DashboardViewController.h"
 #import <LGSideMenuController/UIViewController+LGSideMenuController.h>
 #import <ZLSwipeableView/ZLSwipeableView.h>
+#import "CardView.h"
 
-@interface DashboardViewController ()
+@interface DashboardViewController ()<ZLSwipeableViewDataSource, ZLSwipeableViewDelegate>
 
+@property (weak, nonatomic) IBOutlet ZLSwipeableView *swipeContentView;
 @end
 
 @implementation DashboardViewController
 
 - (void)viewDidLoad {
     self.navigationController.navigationBarHidden = FALSE;
+    // Required Data Source
+    self.swipeContentView.dataSource = self;
+    // Optional Delegate
+    self.swipeContentView.delegate = self;
+    
     [super viewDidLoad];
+}
+
+- (void)viewDidLayoutSubviews {
+    [self.swipeContentView loadViewsIfNeeded];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+#pragma mark - ZLSwipeableViewDelegate
+
+- (void)swipeableView:(ZLSwipeableView *)swipeableView
+         didSwipeView:(UIView *)view
+          inDirection:(ZLSwipeableViewDirection)direction {
+    NSLog(@"did swipe in direction: %zd", direction);
+}
+
+- (void)swipeableView:(ZLSwipeableView *)swipeableView didCancelSwipe:(UIView *)view {
+    NSLog(@"did cancel swipe");
+}
+
+- (void)swipeableView:(ZLSwipeableView *)swipeableView
+  didStartSwipingView:(UIView *)view
+           atLocation:(CGPoint)location {
+    NSLog(@"did start swiping at location: x %f, y %f", location.x, location.y);
+}
+
+- (void)swipeableView:(ZLSwipeableView *)swipeableView
+          swipingView:(UIView *)view
+           atLocation:(CGPoint)location
+          translation:(CGPoint)translation {
+    NSLog(@"swiping at location: x %f, y %f, translation: x %f, y %f", location.x, location.y,
+          translation.x, translation.y);
+}
+
+- (void)swipeableView:(ZLSwipeableView *)swipeableView
+    didEndSwipingView:(UIView *)view
+           atLocation:(CGPoint)location {
+    NSLog(@"did end swiping at location: x %f, y %f", location.x, location.y);
+}
+
+#pragma mark - ZLSwipeableViewDataSource
+
+- (UIView *)nextViewForSwipeableView:(ZLSwipeableView *)swipeableView {
+    CardView *contentView =
+    [[NSBundle mainBundle] loadNibNamed:@"CardView" owner:self options:nil][0];
+    contentView.frame = swipeableView.bounds;
+    return contentView;
+}
+
 
 - (void) initializeView {
     self.title = @"Muslims Of 2 Worlds";
